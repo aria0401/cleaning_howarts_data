ca417ea6f722f35382d94365592a06981f80236f
Ariadna <aria0401@kea.stud.dk>
2020-09-06 00:17:33 +0200

    firstName ok

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 "use strict";
 window.addEventListener("load", start);
+const studensList = [];
+
+const Students = {
+  firstName: "",
+  lastName: "",
+  middleName: "",
+  nickName: "",
+  photoName: "",
+  house: "",
+};
 
 function start() {
   getJsonData();
 }
 
-function getJsonData() {}
+async function getJsonData() {
+  const URL = "https://petlatkea.dk/2020/hogwarts/students.json";
+  const jsonData = await fetch(URL);
+  const studensArray = await jsonData.json();
+  getStudentsObject(studensArray);
+  console.log(studensArray);
+}
+
+function getStudentsObject(studensArray) {
+  studensArray.forEach((element) => {
+    const studens = Object.create(Students);
+    const fullName = element.fullname;
+    const nameLowerCase = fullName.trim().toLowerCase();
+    let capital = nameLowerCase.substring(0, 1).toUpperCase();
+    let name = nameLowerCase.substring(1, nameLowerCase.indexOf(" "));
+    const firstName = `${capital}${name}`;
+    console.log(firstName);
+    console.log(nameLowerCase);
+
+    studens.firstName = firstName;
+    studensList.push(studens);
+  });
+
+  console.table(studensList);
+  orderTheList();
+}
+
+function orderTheList() {}
